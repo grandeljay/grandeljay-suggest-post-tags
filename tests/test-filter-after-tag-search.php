13b2794a34e20ce66d46b97b40971f521bada8bf
grandeljay <github.jay@grandel.anonaddy.me>
2022-04-28 11:01:54 +0200

    Improve after tag search test

--- tests/test-filter-after-tag-search.php
+++ tests/test-filter-after-tag-search.php
@@ -1,14 +1,34 @@
 <?php
-class ArrayTest extends WP_UnitTestCase {
+/**
+ * Test wp_after_tag_search filter
+ *
+ * @package SuggestPostTags
+ * @author  Jay Trees <github.jay@grandel.anonaddy.me>
+ */
 
+/**
+ * Test class
+ */
+class GrandeljaySuggestPostTagsAfterTagSearch extends WP_UnitTestCase {
+
+	/**
+	 * Test if results is an array containing string values.
+	 */
 	public function test_grandeljay_suggest_post_tags_after_tag_search() {
-		$results = array();
+		$results = array(
+			'hello',
+			'3454',
+		);
 		$tax     = new WP_Taxonomy( 'taxonomy-key', 'post_tag' );
 		$s       = 'Search query';
 
-		$this->assertIsArray(
-			grandeljay_suggest_post_tags_after_tag_search( $results, $tax, $s )
-		);
+		$results = grandeljay_suggest_post_tags_after_tag_search( $results, $tax, $s );
+
+		$this->assertIsArray( $results );
+
+		foreach ( $results as $result ) {
+			$this->assertIsString( $result );
+		}
 	}
 
 }
